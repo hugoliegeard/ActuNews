2f1647a77e07b8c346d9bd3315431298bbf6b1ab
Hugo LIEGEARD <hugo@biyn.media>
2019-03-01 15:56:59 +0100

    Update categorie.php
    Affichage des articles d'une catégorie

--- categorie.php
+++ categorie.php
@@ -1,34 +1,60 @@
 <?php require_once('inc/init.inc.php'); ?>
 
-<!--
-Grâce à PHP, nous allons utiliser le même fichier
-pour plusieurs catégories.
+    <!--
+    Grâce à PHP, nous allons utiliser le même fichier
+    pour plusieurs catégories.
 
-Avec un site statique nous aurions été obligé
-de créer un fichier par catégorie : politique.html economie.html sports.html ...
--->
+    Avec un site statique nous aurions été obligé
+    de créer un fichier par catégorie : politique.html economie.html sports.html ...
+    -->
 
-<!-- ----------------------------- | TRAITEMENT | ------------------------------ -->
+    <!-- ----------------------------- | TRAITEMENT | ------------------------------ -->
 
 <?php
 
-    // -- Récupération de l'id de la catégorie
-    $id_categorie = $_GET['id_categorie'];
-    $nom_categorie = $_GET['nom_categorie'];
+// -- Récupération de l'id de la catégorie
+$id_categorie = (isset($_GET['id_categorie'])) ? $_GET['id_categorie'] : '';
+$nom_categorie = (isset($_GET['nom_categorie'])) ? $_GET['nom_categorie'] : '';
+
+// -- Récupération en BDD
+if (isset($_GET['id_categorie'])) {
+    $request = $pdo_connexion->query("SELECT * FROM article WHERE IDCATEGORIE = '$_GET[id_categorie]'");
+}
+
+// -- Si aucun résultat
+if ($request->rowCount() <= 0) {
+    $page_content .= '<div class="alert alert-warning">
+                            Aucune publication pour le moment.   
+                         </div>';
+} else {
+    $page_content .= '<div class="row">';
+    while ($article = $request->fetch(PDO::FETCH_ASSOC)) {
+        $page_content .= '<div class="col-sm-4 mb-4">
+                              <div class="card">
+                                  <div class="card-body">
+                                    <h5 class="card-title">' . $article['TITREARTICLE'] . '</h5>
+                                    <a href="#" class="btn btn-primary">Consulter</a>
+                                  </div>
+                              </div>     		
+                          </div>';
+    }
+    $page_content .= '</div>';
+}
 
 ?>
 
-<!-- ----------------------------- | AFFICHAGE | ------------------------------ -->
+    <!-- ----------------------------- | AFFICHAGE | ------------------------------ -->
 
 <?php require_once('inc/header.inc.php'); ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h1>PAGE CATEGORIE</h1>
-            <h3><?= $id_categorie ?> - <?= $nom_categorie ?></h3>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="mb-4"><?= ucfirst($nom_categorie) ?></h1>
+                <?= $page_content ?>
+            </div>
         </div>
     </div>
-</div>
 
 <?php require_once('inc/footer.inc.php'); ?>
+
